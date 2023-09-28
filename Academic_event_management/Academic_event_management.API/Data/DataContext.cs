77861bf83f7dec0b050ba80bbffb21fdb922b974
JUAN PABLO SERNA JARAMILLO <juan.serna@sistemasentry.com.co>
2023-09-28 01:02:44 -0500

    We create "AcademicEvent" and "Atendee" entities with their respective controllers and CRUD operations.
    We create base files like DataContext in API project, repositories folder in WEB project, etc.

---
+++ Academic_event_management/Academic_event_management.API/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Academic_event_management.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Academic_event_management.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<AcademicEvent> AcademicEvents { get; set; }
+        public DbSet<Attendee> Attendees { get; set; }
+
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
