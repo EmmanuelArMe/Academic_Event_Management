8e91d1993dcd4687b2d9994c0b20fdbcf4028a46
JUAN PABLO SERNA JARAMILLO <juan.serna@sistemasentry.com.co>
2023-09-28 01:02:44 -0500

    Se crean entidades "AcademicEvent" y "Atendee" con sus respectivos controladores y operaciones CRUD.
    Se crean archivos bases como DataContext en el API, carpeta Repositories en el proyecto WEB, etc.

---
+++ Academic_event_management/Academic_event_management.API/Controllers/AttendeesController.cs
@@ -0,0 +1,87 @@
+﻿using Academic_event_management.API.Data;
+using Academic_event_management.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Academic_event_management.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AttendeesController : ControllerBase
+    {
+        public DataContext _context;
+
+        public AttendeesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        //Get an attendee.
+        [HttpGet("{Id}")]
+        public async Task<ActionResult> Get(int Id)
+        {
+            var Attendee = await _context.Attendees.FirstOrDefaultAsync(x => x.Id == Id);
+
+            if (Attendee == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(Attendee);
+            }
+        }
+
+        //Get a list of attendees.
+        [HttpGet]
+        public async Task<ActionResult> GetList()
+        {
+            var AttendeesList = await _context.Attendees.ToListAsync();
+
+            if (AttendeesList.Count == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(AttendeesList);
+            }
+        }
+
+        //Insert an attendee.
+        [HttpPost]
+        public async Task<ActionResult> Post(Attendee Attendee)
+        {
+
+            _context.Add(Attendee);
+            await _context.SaveChangesAsync();
+            return Ok(Attendee);
+        }
+
+        //Update an attendee.
+        [HttpPut]
+        public async Task<ActionResult> Put(Attendee Attendee)
+        {
+
+            _context.Update(Attendee);
+            await _context.SaveChangesAsync();
+            return Ok(Attendee);
+        }
+
+        //Delete an attendee.
+        [HttpDelete("{Id}")]
+        public async Task<ActionResult> Delete(int Id)
+        {
+            var Attendee = await _context.Attendees.Where(x => x.Id == Id).ExecuteDeleteAsync();
+
+            if (Attendee == 0)
+            {
+
+                return NotFound();
+
+            };
+
+            return NoContent();
+        }
+    }
+}
--- Academic_event_management/Academic_event_management.WEB/Program.cs
+++ Academic_event_management/Academic_event_management.WEB/Program.cs
@@ -6,6 +6,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+// HttpClient para consumir el API desde la Web.
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7000/") });
 
 await builder.Build().RunAsync();
